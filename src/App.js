d63557ab4fb274b699d7ca68a85888058dcec55f
kieudoannam <kieunam2808@gmail.com>
2023-02-10 20:05:47 +0700

    move ShopContext.Provider from App.js to  index.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Shop from './pages/shop/shop';
 import Cart from './pages/cart/cart';
-import { ShopContextProvider } from './context/shop-context';
 
 function App() {
   return (
     <div className="App">
-      <ShopContextProvider>
         <Router>
           <Navbar/>
           <Routes>
@@ -16,7 +14,6 @@ function App() {
             <Route path='/cart' element={<Cart/>} />
           </Routes>
         </Router>
-      </ShopContextProvider>
     </div>
   );
 }
